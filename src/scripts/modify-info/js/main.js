edc22543f498bba254fe690b4f599845967b5c13
basiczombie <issacgable@gmail.com>
2017-01-04 16:24:17 -0700

    fixed entry location

--- src/scripts/modify-info/js/main.js
+++ src/scripts/modify-info/js/main.js
@@ -1,31 +1,34 @@
-/*global $j*/
+import $ from 'jquery';
 
-(function() {
-    'use strict';
-    var accessTemplate = $j($j('#access-template').html());
-    var select = $j('tr').eq(-2);
-    accessTemplate.insertAfter(select);
+/*global $*/
+export default function() {
+    $(() => {
+        var accessTemplate = $($('#access-template').html());
+        var select = $('tr').eq(-4);
+        console.log(select);
+        accessTemplate.insertAfter(select);
 
-    var advisorTemplate = $j($j('#advisor-template').html());
-    select = $j('tr').eq(2);
-    advisorTemplate.insertAfter(select);
+        var advisorTemplate = $($('#advisor-template').html());
+        select = $('tr').eq(2);
+        advisorTemplate.insertAfter(select);
 
-    var nicknameTemplate = $j($j('#nickname-template').html());
-    select = $j('tr').eq(0);
-    nicknameTemplate.insertAfter(select);
+        var nicknameTemplate = $($('#nickname-template').html());
+        select = $('tr').eq(0);
+        nicknameTemplate.insertAfter(select);
 
-    var enrollmentStatusTemplate = $j($j('#enrollment-status-template').html());
-    select = $j('tr').eq(5);
-    enrollmentStatusTemplate.insertAfter(select);
+        var enrollmentStatusTemplate = $($('#enrollment-status-template').html());
+        select = $('tr').eq(5);
+        enrollmentStatusTemplate.insertAfter(select);
 
-    var stuPassTemplate = $j($j('#stu-pass-template').html());
+        var stuPassTemplate = $($('#stu-pass-template').html());
 
-    // User is in group 50, so the template was picked up by jQuery
-    if (stuPassTemplate.length > 0) {
-        select = $j('tr').eq(-2);
-        stuPassTemplate.insertAfter(select);
-    }
+        // User is in group 50, so the template was picked up by jQuery
+        if (stuPassTemplate.length > 0) {
+            select = $('tr').eq(-4);
+            stuPassTemplate.insertAfter(select);
+        }
 
-    // Fix row coloring
-    $j('.box-round').find('tr').attr('bgcolor','');
-}());
+        // Fix row coloring
+        $('.box-round').find('tr').attr('bgcolor','');
+    });
+}
